b1aa315085288f3a35d1c8c48b5c0d6fd0113712
eblish159 <tkdgud159@naver.com>
2026-06-12 18:03:24 +0900

    Add mapper scan

--- src/main/java/com/myplan/myplan/myplan/MyplanApplication.java
+++ src/main/java/com/myplan/myplan/myplan/MyplanApplication.java
@@ -1,13 +1,14 @@
-package com.myplan.myplan.myplan;
+package com.myplan.myplan;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.myplan.myplan.myplan.plan.dao")
 public class MyplanApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MyplanApplication.class, args);
     }
-
 }
